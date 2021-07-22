f72f46aadd06eb10d126447217114aa3f944a5e2
manu <ivianuu@gmail.com>
2021-07-23 00:56:12 +0200

    Updated copyright

--- app/src/main/kotlin/com/ivianuu/playlistshuffler/api/AuthUi.kt
+++ app/src/main/kotlin/com/ivianuu/playlistshuffler/api/AuthUi.kt
@@ -1,17 +1,19 @@
 /*
- * Copyright 2020 Manuel Wrage
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2021 Manuel Wrage
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package com.ivianuu.playlistshuffler.api
@@ -42,6 +44,7 @@ import com.ivianuu.injekt.coroutines.*
 import com.ivianuu.injekt.scope.*
 import com.ivianuu.playlistshuffler.R
 import com.ivianuu.playlistshuffler.api.*
+import com.ivianuu.playlistshuffler.app.*
 import com.ivianuu.playlistshuffler.home.*
 import com.spotify.sdk.android.auth.*
 import kotlinx.coroutines.flow.*
